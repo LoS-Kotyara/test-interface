1f7b9a2e4b68868c9085fcb3a1f1de7c88d7202c
los_kotyara <aad8a13@gmail.com>
2021-06-06 18:13:11 +0400

    AboutPage fixes

--- src/containers/AboutPage/AboutPage.tsx
+++ src/containers/AboutPage/AboutPage.tsx
@@ -1,7 +1,15 @@
+import styles from './AboutPage.module.css';
+
 const AboutPage = () => {
   return (
     <>
       <h1 className="header__text">About</h1>
+      <div className={styles.content}>
+        {`Web application to control LED strip.
+        
+        Build with React.
+        `}
+      </div>
     </>
   );
 };
